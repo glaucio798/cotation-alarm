d24511caa7ac44db2173e070ecbdaad791873728
Glaucio Martins <glaucionepre@gmail.com>
2021-02-11 13:47:21 -0300

    api

--- teste.py
+++ teste.py
@@ -3,14 +3,18 @@ import time
 from playsound import playsound
 import sys
 
-client = Client('xkOqNAojzWlATB2b', 't6pFXekGdWasj6dPntciPOzWMSzBAUvn')
+API_key = "your_api_here"
+API_secret = "your_api_secret_here"
+coin = "coin_code_here"
+
+client = Client(API_key, API_secret)
 value = float(sys.argv[2])
 state = sys.argv[1]
 if(len(sys.argv)>3):
     value2 = float(sys.argv[3])
 
 while True:
-    currencies = client.get_buy_price(currency_pair = 'FIL-BRL');
+    currencies = client.get_buy_price(currency_pair = coin);
     print(currencies['amount'])
     if(state == 'abaixo'):
         if(float(currencies['amount']) <= value):
